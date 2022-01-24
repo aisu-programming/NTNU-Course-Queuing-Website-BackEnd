c33bd1dd33d5462a08a9179de691b52f7b1a698a
aisu-programming <aisu.programming@gmail.com>
2022-01-24 23:23:34 +0800

    Update

--- validation/model.py
+++ validation/model.py
@@ -16,7 +16,7 @@ gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     try:
         tf.config.experimental.set_virtual_device_configuration(
-            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
+            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)]
         )
     except RuntimeError as e:
         print(e)
